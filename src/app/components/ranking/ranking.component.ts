a6d170a69cef265e2619a53fcd34adbe50adb9e3
pedroDulce <pedro.dulce@gmail.com>
2025-11-13 19:09:45 +0100

    .

--- src/app/components/ranking/ranking.component.ts
+++ src/app/components/ranking/ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QaApiService, RankingDTO } from '../../services/qa-api.service';
+import { QaApiService } from '../../services/qa-api.service';
+import { RankingDTO } from '../../models/chat.model';
 
 @Component({
   selector: 'app-ranking',
